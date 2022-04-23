533aeb5a8a9003a5f63d7fdea4b539d13962daa1
Dymitr Lubczyk <dymitrlubczyk@gmail.com>
2022-04-23 19:59:42 +0200

    missing file

--- src/merkle-tree.test.ts
+++ src/merkle-tree.test.ts
@@ -1,7 +1,6 @@
 import { Field, shutdown } from "snarkyjs"
 import { MerkleTree } from './merkle-tree'
 describe('Merkle Tree', () => {
-<<<<<<< HEAD
     afterAll(async () => {
         await shutdown();
       });
@@ -10,10 +9,6 @@ describe('Merkle Tree', () => {
         console.log('adsdas')
         Field.one
         // const fields = [new Field(0), new Field(1), new Field(2)]
-=======
-    it('builds correct tree', () => {
-        const fields = [new Field(0), new Field(1), new Field(2)]
->>>>>>> ac69782... Use mocha for tests
         // const tree = new MerkleTree(fields)
         // console.log(tree.nodes);
     })
